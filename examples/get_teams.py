0ab876ef5f336e6d5c68e62ee0ca2b4a813967b6
Jordane Titus Gengo <jordane@hive.fi>
2021-01-25 07:37:40 +0200

    Edit get_teams example

--- examples/get_teams.py
+++ examples/get_teams.py
@@ -1,12 +1,13 @@
 #Note that you can import already created instance of IntraAPIClient from intra.py
 from intra import ic
+from config import campus_id
 
 import json
-import config
+
 
 #Instead of writing everything in .get, you can create a payload
 payload = {
-    'filter[primary_campus_id]': config.campus_id
+    'filter[primary_campus_id]':campus_id
 }
 
 #You can shorten the url if you have properly configured your config.yml
